16fbb89e1354dd9a8af5f99372f961ba649083b6
Eric Bower <neurosnap@gmail.com>
2015-05-20 16:45:49 -0400

    fixed gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,8 @@ var less = require('gulp-less');
 var uglify = require('gulp-uglify');
 var sourcemaps = require('gulp-sourcemaps');
 var babel = require('gulp-babel');
+var babelify = require("babelify");
+var reactify = require('reactify');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
@@ -38,12 +40,16 @@ gulp.task('browserify', function() {
 
 gulp.task('watch', function() {
   forEach(jsBundle, function(fname) {
-    gutil.log('Watching ' + fname);
+    gutil.log('Watching ' + fname + ' ...');
     gulp.watch(jsSrc + fname, function() {
       bundlejs(fname);
     });
   });
 
+  gutil.log('Watching node modules ...');
+  gulp.watch('./src/**/*.js', ['babel']);
+
+  gutil.log('Watching less files ...');
   gulp.watch('./public/less/**/*.less', ['less']);
 });
 
@@ -51,7 +57,7 @@ gulp.task('babel', function() {
   var src = './src/**/*.js';
   var dist = './dist';
 
-  gutil.log('Generating ES6 -> ES5 files');
+  gutil.log('Generating ' + src + ' files to ' + dist + ' ...');
 
   return gulp.src(src)
     .pipe(babel({ stage: 0 }))
@@ -79,14 +85,17 @@ function bundlejs(file, src, dist) {
   gutil.log('Generating ' + distFull);
 
   var b = browserify(srcFull, { debug: true });
-  return b.bundle()
+  return b.transform(reactify)
+    .bundle()
     .pipe(source(file))
+    .on('error', gutil.log)
+    .pipe(gulp.dest(dist));
     // .pipe(buffer())
     // .pipe(sourcemaps.init({loadMaps: true}))
     //     .pipe(uglify())
     //     .on('error', gutil.log)
     // .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest(dist));
+    //.pipe(gulp.dest(dist));
 }
 
 function addSlash(path) {
