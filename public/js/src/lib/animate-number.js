6d302b94c55d629dfa27a91ccb54d620af5c43d3
Eric Bower <neurosnap@gmail.com>
2015-05-22 10:33:48 -0400

    use strict

--- public/js/src/lib/animate-number.js
+++ public/js/src/lib/animate-number.js
@@ -1,10 +1,11 @@
+'use strict';
+
 import React from 'react';
 import ReactNumberEasing from 'react-number-easing';
 
 module.exports = function(value, id) {
-
   return React.render(
     <ReactNumberEasing value={ value }/>,
     document.getElementById(id)
   )
-}
+};
