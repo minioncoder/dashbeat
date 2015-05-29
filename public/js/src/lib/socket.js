b78b6c320938ef6e5f651275e7ba6f7c740a5811
Eric Bower <neurosnap@gmail.com>
2015-05-29 11:42:39 -0400

    jsdocs

--- public/js/src/lib/socket.js
+++ public/js/src/lib/socket.js
@@ -1,8 +1,24 @@
+/**
+* @module socket
+*/
 'use strict';
 
 import io from 'socket.io-browserify';
 
+/**
+* DashSocket - Simple interface for connecting and interacting with socket.io
+*
+* @class
+*/
 export default class DashSocket {
+  /**
+  * @constructs
+  * @param {String|Array} [events] Events to emit on init
+  * @param {Object} Options
+  * @return {Object} The DashSocket instance
+  * @example
+  *   var dash = new DashSocket('toppages');
+  */
   constructor(events, { ondisconnect } = {}) {
     Object.assign(this, { events });
     this.rooms = {};
@@ -11,6 +27,12 @@ export default class DashSocket {
     this.dc(ondisconnect);
   }
 
+  /**
+  * Emits an event that hooks into a Beat socket route which corresponds
+  * with a room
+  *
+  * @memberof DashSocket#
+  */
   emit(events) {
     if (typeof events === 'undefined') events = this.events;
 
@@ -27,6 +49,12 @@ export default class DashSocket {
     }
   }
 
+  /**
+  * Handles what to do when socket disconnects
+  *
+  * @memberof DashSocket#
+  * @param {Function} [dcb] On disconnect callback
+  */
   dc(dcb) {
     if (typeof dcb === 'undefined') {
       dcb = data => {
@@ -39,20 +67,49 @@ export default class DashSocket {
     this.socket.on('disconnect', dcb);
   }
 
+  /**
+  * Grabs the Room# instance by name
+  *
+  * @memberof DashSocket#
+  * @param {String} [name] The name of the room
+  * @return {Object} The Room# instance
+  */
   room(name) {
     return this.rooms[name];
   }
 
+  /**
+  * Quick and dirty logger with timestamp for all socket logs
+  *
+  * @memberof DashSocket#
+  * @param {String} [msg] The message to send to console.log
+  */
   log(msg) {
     console.log(new Date().toString() + ': ' + msg);
   }
 }
 
+/**
+* Room - Simple interface for room events.  This class allows us to specify
+* a room to listen for an event on.
+*
+* @class
+*/
 class Room {
+  /**
+  * @constructs
+  * @param {Object} [socket] The socket instance, ex var socket = io.connect()
+  * @param {String} [name] The name of the room to join
+  */
   constructor(socket, name) {
     Object.assign(this, { socket, name });
   }
 
+  /**
+  * @constructs
+  * @param {String} [event_name] The name of the event, ex. 'message'
+  * @param {Function} [cb] The event handler callback
+  */
   on(event_name, cb) {
     this.socket.on(this.name + '-' + event_name, cb);
   }
