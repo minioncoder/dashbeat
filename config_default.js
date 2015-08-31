08e2ed067a9fce8dfa14f1dd89dd165072ad022b
Eric Bower <neurosnap@gmail.com>
2015-08-31 11:57:24 -0400

    added new default config for dev

---
+++ config_default.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apikey: '',
+  hosts: [
+    "detroitnews.com",
+    "freep.com",
+    "battlecreekenquirer.com",
+    "hometownlife.com",
+    "lansingstatejournal.com",
+    "livingstondaily.com",
+    "thetimesherald.com",
+  ]
+};
