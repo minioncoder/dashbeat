d957d3d47e2909d7c8a8a089558e4b202068389f
Mike Varano <mikevee313@gmail.com>
2015-10-20 11:33:05 -0400

    fixed big picture hopefully for good

--- src/client/big-picture.js
+++ src/client/big-picture.js
@@ -1,16 +1,37 @@
 'use strict';
 
 import io from 'socket.io-client';
+import xr from 'xr';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 import ArticleHandler from './big-picture/article-handler';
 
 var socket = io('https://api.michigan.com', { transports: ['websocket', 'xhr-polling'] })
-//var articleHandler = new ArticleHandler();
-//var articleContainer = document.getElementsByClassName('articles')[0];
 
 ReactDOM.render(
-  <ArticleHandler />,
+  <ArticleHandler articles={ [] }/>,
   document.getElementById('articles')
 )
+
+function fetchArticlesLoop() {
+  xr.get('https://api.michigan.com/v1/news', { limit: 100 })
+    .then(res => {
+
+      let articles = [];
+      for (let article of res.articles) {
+        if (!article.photo || !article.photo.full) continue;
+        articles.push(article);
+      }
+
+      ReactDOM.render(
+        <ArticleHandler articles={ articles }/>,
+        document.getElementById('articles')
+      )
+    });
+
+  setTimeout(fetchArticlesLoop, 1000 * 60 * 10);
+}
+
+
+fetchArticlesLoop();
